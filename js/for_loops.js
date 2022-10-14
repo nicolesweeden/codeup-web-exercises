71a62790779a48d825fca6b63c815082a3026b79
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-10-14 15:01:41 -0600

    completed

--- js/for_loops.js
+++ js/for_loops.js
@@ -20,17 +20,13 @@
 
 // #3: Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
-// for (let i = 0; i <= 10; i = Math.floor(Math.random() * 201)) {
-//     if (i % 2 !== 0) {
-//         console.log("The number" + num + " is even!");
-//     } else {
-//         console.log("The number" + num + " is odd!");
-//     }
 
-
-    for (let i = 0; i <= 10; i++) {
-        if (i % 2 !== 0) {
-            console.log("The number" + num + " is even!");
+function exerciseThree() {
+    for (let i = 0; i <= 10; i++) { //establishes value of our iterator; condition bc i<=10; change that happens to iterator
+        let num = Math.floor(Math.random() * (201 - 20)) + 20; //new num variable w a differnet value
+        if (num % 2 == 0) {
+            console.log("The number " + num + " is even!");
         } else {
-            console.log("The number" + num + " is odd!");
-        }}
+            console.log("The number " + num + " is odd!");
+        }}}
+exerciseThree() //this is the function call
