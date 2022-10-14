2227e476f21ffd3392e1f2e3f05ffa158098ec50
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-10-14 15:02:30 -0600

    completed

---
+++ js/break_and_continue.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Create a file named break_and_continue.js in the js directory.
+//
+//     Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+//
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+let userInput = prompt("Enter an odd number between 1 and 50")
+//check if number is odd
+if(userInput % 2 !== 0) {
+    console.log("The number " + userInput + " is odd.");
+}
+for (var i = 1; i <=50 ; i++) {
+
+    if (i % 2 !== 0) {
+        continue;
+    }
+
+    console.log('Here is a lovely even number: ' + i);
+}
--- js/for_loops.js
+++ js/for_loops.js
@@ -21,12 +21,52 @@
 // #3: Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
 
-function exerciseThree() {
-    for (let i = 0; i <= 10; i++) { //establishes value of our iterator; condition bc i<=10; change that happens to iterator
-        let num = Math.floor(Math.random() * (201 - 20)) + 20; //new num variable w a differnet value
-        if (num % 2 == 0) {
-            console.log("The number " + num + " is even!");
-        } else {
-            console.log("The number " + num + " is odd!");
-        }}}
-exerciseThree() //this is the function call
+// function exerciseThree() {
+//     for (let i = 0; i <= 10; i++) { //establishes value of our iterator; condition bc i<=10; change that happens to iterator
+//         let num = Math.floor(Math.random() * (201 - 20)) + 20; //new num variable w a differnet value
+//         if (num % 2 == 0) {
+//             console.log("The number " + num + " is even!");
+//         } else {
+//             console.log("The number " + num + " is odd!");
+//         }}}
+// exerciseThree() //this is the function call
+
+
+//#4 Create a for loop that uses console.log to create the output shown below.
+//
+//
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 99999999
+
+// for (var i = 1; i <=9 ; i++) {
+//     console.log(i)
+// }
+//=========
+// var divLength = ('div').length;
+//
+// var str = '';
+// for(var i = 0; i < divLength; i++) {
+//     str += "Div #" + i + ", ";
+//     console.log(str);
+// }
+//=========
+// i cant figure out how to get this one to show the nu,bers correctly, they're coming out as 1, 12, 123, 1234, etc. but this is my final answer for #4
+// let str = "";
+// for (let i = 1; i <=9 ; i++) {
+//     str += "" + i;
+//     console.log(str)
+// }
+
+//#5 OMG it worked!
+let i = "";
+for (i = 100; i > 0; i-=5) {
+    console.log(i);
+}
+
