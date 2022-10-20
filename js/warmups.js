98b9c74c8cafed1ffee77c9efd45178f4ebc79e6
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-10-20 08:22:15 -0600

    warmup 1

---
+++ js/warmups.js
@@ -0,0 +1,25 @@
+(function(){
+    "use strict";
+
+
+
+    // Write a function that when passed an object will return the value of the object’s price property.
+    //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+    // getPrice(obj); // returns “$7.89”
+
+    // let apples = new Object();
+    // apples.price = "1.99"
+    // getPrice(apples)
+
+    function price(object) {
+        return object.price
+    }
+
+    let apples = {
+        name: "apples"
+        quantity: "3"
+        price: "1.99"
+    }
+    console.log(getPrice(apples));
+
+})();
