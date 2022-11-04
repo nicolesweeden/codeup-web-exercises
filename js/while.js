e008b22f588da215653bbef0515c142417bbc232
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-11-04 09:24:22 -0600

    completed while exercise

--- js/while.js
+++ js/while.js
@@ -19,16 +19,22 @@
 // 32768
 // 65536
 
+var a = 1;
+var b = 0;
 
+while(b < 16){
+a *= 2;
+b++
+    console.log(a);
+}
+var x = 1;
+while(x < 65536){
+    console.log(x *= 2);
+}
 
 
 // Do While Loop
 // An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
-//
-// // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// // This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
 //
 // The output should be similar to the following:
 //
@@ -37,3 +43,18 @@
 //     Yay! I sold them all! // If there are no more cones
 //
 // Finally, commit the changes to your git repository, and push to GitHub.
+
+var iceCreamCones = Math.floor(Math.random() * 50) + 50;
+Math.floor(Math.random() * 5) + 1;
+
+do {
+    var conesToBuy = Math.floor(Math.random() * 5) + 1;
+    if((iceCreamCones - conesToBuy) < 0) {
+        console.log("Cannot sell you " + conesToBuy + " cones. I only have " + iceCreamCones + ".");
+    } else {
+        console.log("# of cones before purchase: " + iceCreamCones);
+        iceCreamCones -= conesToBuy;
+        console.log(conesToBuy + " cones sold.");
+    }
+} while (iceCreamCones > 0);
+console.log("Yay! I sold them all!");
